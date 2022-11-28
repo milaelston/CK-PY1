3f7f540340f8beede4d4eb7aa974457fdfc4ba44
milaelston <115098594+milaelston@users.noreply.github.com>
2022-11-28 19:24:42 +0300

    Лабораторная работа 6 исправленная
    Исправила недочет с docstring.

---
+++ task4.py
@@ -0,0 +1,19 @@
+import json
+
+INPUT_FILE = "input.csv"
+
+
+def csv_to_list_dict(filename: str, delimiter: str = ',', linesep: str = '\n') -> list[dict]:
+    with open(filename) as f:
+        rows = f.read().strip("\n").split(linesep)
+        listofdicts = []
+        columnnames = rows[0].split(delimiter)
+        for row in range(1, len(rows)):
+            currentrownums = rows[row].split(delimiter)
+            numrow = len(currentrownums)
+            listofdicts.append({columnnames[idx]: currentrownums[idx] for idx in range(numrow)})
+
+        return listofdicts
+
+
+print(json.dumps(csv_to_list_dict(INPUT_FILE), indent=4))
